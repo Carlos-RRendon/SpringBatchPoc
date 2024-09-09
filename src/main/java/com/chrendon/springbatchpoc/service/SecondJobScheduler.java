f06f197354c4327d265764f9144e16e2836b1657
CharlesR.Rendon <carlos.ramirezrn@outlook.com>
2024-09-09 16:55:43 -0600

    Chunk - Se añade la funcionalidad de scheduler

---
+++ src/main/java/com/chrendon/springbatchpoc/service/SecondJobScheduler.java
@@ -0,0 +1,44 @@
+package com.chrendon.springbatchpoc.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+
+public class SecondJobScheduler {
+
+
+    private final JobLauncher jobLauncher;
+
+
+    private final Job secondJob;
+
+    public SecondJobScheduler(JobLauncher jobLauncher, @Qualifier("secondJob") Job secondJob) {
+        this.jobLauncher = jobLauncher;
+        this.secondJob = secondJob;
+    }
+
+    @Scheduled(cron = "0 * * * * *")
+    public void secondJobScheduler() {
+
+        JobParametersBuilder jobParametersBuilder = new JobParametersBuilder();
+        jobParametersBuilder.addLong("currentTime", System.currentTimeMillis());
+        JobParameters jobParameters = jobParametersBuilder.toJobParameters();
+        try {
+            JobExecution jobExecution = jobLauncher.run(secondJob, jobParameters);
+            log.debug("Job Execution ID: {}", jobExecution.getId());
+        } catch (JobInstanceAlreadyCompleteException | JobExecutionAlreadyRunningException |
+                 JobParametersInvalidException | JobRestartException e) {
+            log.error("Error executing job", e);
+        }
+
+    }
+}
